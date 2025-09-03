8d5f09ed97d579d22d9858cdc3c06971348b157e
Thodoris Sairoglou <theodoros.sairoglou@bbv.gr>
2025-09-03 19:49:22 +0300

    merge done

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
 
 // Fix __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
@@ -10,17 +10,17 @@ const __dirname = path.dirname(__filename);
 export default defineConfig(() => {
   return {
     plugins: [react()],
-    base: '/',
+    base: "/",
     server: {
       open: true,
       port: 3000,
     },
     build: {
-      outDir: './build',
+      outDir: "./build",
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, 'src'),
+        "@": path.resolve(__dirname, "src"),
       },
     },
   };
